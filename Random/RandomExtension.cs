ba8ed9901e229f7711e744ccb783a28bc0e2be1e
Aplem6715 <Aplem6715@gmail.com>
2023-11-23 16:23:48 +0900

    RandomExtensionのリファクタリング

--- Random/RandomExtension.cs
+++ Random/RandomExtension.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
-using Unity.Mathematics;
+﻿using Unity.Mathematics;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace Aplem.Common
 {
@@ -9,7 +9,7 @@ namespace Aplem.Common
 
     public static class RandomExtension
     {
-        public delegate float GetWeightFunc<T>(T item);
+        public delegate float GetWeightFunc<in T>(T item);
 
         /// <summary>
         /// 入力されたRandomを元に新たなRandomを生成
@@ -25,6 +25,7 @@ namespace Aplem.Common
         /// 正規分布に従う乱数を生成
         /// </summary>
         /// <param name="rand">乱数生成器</param>
+        /// <param name="mean">平均</param>
         /// <param name="std">標準偏差</param>
         public static float NextNormal(this ref Random rand, float mean, float std)
         {
@@ -48,6 +49,7 @@ namespace Aplem.Common
         /// <summary>
         /// 小数点以下の値に応じてランダムに丸める
         /// </summary>
+        /// <param name="rand">random</param>
         /// <param name="value">丸める値</param>
         /// <returns>丸めた値</returns>
         public static int Round(this ref Random rand, float value)
@@ -59,27 +61,25 @@ namespace Aplem.Common
         /// <summary>
         /// 配列をシャッフルする
         /// </summary>
+        /// <param name="rand">random</param>
         /// <param name="array">シャッフルする配列</param>
-        public static void Shuffle<T>(this ref Random rand, ref T[] array)
+        private static void Shuffle<T>(this ref Random rand, ref T[] array)
         {
             for (var i = array.Length - 1; i > 0; i--)
             {
                 var j = rand.NextInt(i + 1);
-                var temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                (array[i], array[j]) = (array[j], array[i]);
             }
         }
 
         /// <summary>
         /// 重み付きランダムでリストのインデックスを取得
         /// </summary>
+        /// <param name="random">random</param>
         /// <param name="weights">重みリスト</param>
         public static int ChoiceWeighted(this ref Random random, IReadOnlyList<float> weights)
         {
-            float sum = 0;
-            foreach (var weight in weights)
-                sum += weight;
+            var sum = weights.Sum();
 
             var rand = random.NextFloat(sum);
             sum = 0;
@@ -96,20 +96,21 @@ namespace Aplem.Common
         /// <summary>
         /// 重み付きランダムで配列のインデックスを取得
         /// </summary>
+        /// <param name="random">random</param>
         /// <param name="array">配列</param>
-        /// <param name="GetWeight">配列の要素から重みを取得する関数</param>
+        /// <param name="getWeight">配列の要素から重みを取得する関数</param>
         /// <returns>インデックス</returns>
-        public static int ChoiceWeighted<T>(this ref Random random, IReadOnlyList<T> array, GetWeightFunc<T> GetWeight)
+        public static int ChoiceWeighted<T>(this ref Random random, IReadOnlyList<T> array, GetWeightFunc<T> getWeight)
         {
             float sum = 0;
             foreach (var item in array)
-                sum += GetWeight(item);
+                sum += getWeight(item);
 
             var rand = random.NextFloat(sum);
             sum = 0;
             for (var i = 0; i < array.Count; i++)
             {
-                sum += GetWeight(array[i]);
+                sum += getWeight(array[i]);
                 if (rand < sum)
                     return i;
             }
@@ -120,21 +121,22 @@ namespace Aplem.Common
         /// <summary>
         /// 重み付き乱数で辞書の要素を取得
         /// </summary>
+        /// <param name="random">random</param>
         /// <param name="dict">辞書</param>
-        /// <param name="GetWeight">辞書のPairから重みを取得する関数</param>
+        /// <param name="getWeight">辞書のPairから重みを取得する関数</param>
         /// <returns>選択されたValue</returns>
         public static V ChoiceWeighted<K, V>(this ref Random random, IReadOnlyDictionary<K, V> dict,
-            GetWeightFunc<KeyValuePair<K, V>> GetWeight)
+            GetWeightFunc<KeyValuePair<K, V>> getWeight)
         {
             float sum = 0;
             foreach (var pair in dict)
-                sum += GetWeight(pair);
+                sum += getWeight(pair);
 
             var rand = random.NextFloat(sum);
             sum = 0;
             foreach (var pair in dict)
             {
-                sum += GetWeight(pair);
+                sum += getWeight(pair);
                 if (rand < sum)
                     return pair.Value;
             }
@@ -169,12 +171,13 @@ namespace Aplem.Common
         /// <summary>
         /// 合計値が指定した値になるように配列を埋める
         /// </summary>
+        /// <param name="rand">random</param>
         /// <param name="result">結果を格納する配列</param>
         /// <param name="sum">生成する乱数の合計値</param>
         /// <param name="maxPerItem">要素ごとの最大値</param>
         public static void FillDistribute(this ref Random rand, ref int[] result, int sum, int maxPerItem)
         {
-            var remain = sum;
+            // var remain = sum;
             var numItems = result.Length;
 
             // for (int i = 0; i < numItems - 1; i++)
