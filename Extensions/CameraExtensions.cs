e0ea4b11d074f0f3cebdf2fc6741e4ff9e76d606
Aplem6715 <Aplem6715@gmail.com>
2023-12-13 00:51:53 +0900

    CameraExtensions.csの追加

---
+++ Extensions/CameraExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Aplem.Common.Extensions
+{
+    public static class CameraExtensions
+    {
+        public static Vector2 GetWorldPosition2D(this Camera camera, Vector2 screenPosition)
+        {
+            return camera.ScreenToWorldPoint(screenPosition);
+        }
+    }
+}
