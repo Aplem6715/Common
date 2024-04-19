b0f21f6062eb0daa8e0c35be20978e516161879c
Aplem6715 <Aplem6715@gmail.com>
2024-04-20 01:03:39 +0900

    Singletonがリセットされない問題を修正

--- Singleton.cs
+++ Singleton.cs
@@ -1,26 +1,58 @@
+using System.Collections.Generic;
 using UnityEngine;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 
 namespace Aplem.Common
 {
+    public interface ISingletonBase
+    {
+        public void ResetInner();
+    }
+    
     public interface ISingleton
     {
         public void Reset();
     }
 
-    public abstract class Singleton<T> : ISingleton where T : ISingleton, new()
+    public static class SingletonCleaner
+    {
+        private static List<ISingletonBase> _singletons = new();
+        
+        public static void Register(ISingletonBase singleton)
+        {
+            _singletons.Add(singleton);
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void Reset()
+        {
+            // Resetにより新たなsingletonがRegisterされ，_singletonsに追加されるため
+            // 先にコピー＆クリアしてからリセット
+            var oldSingletons = _singletons.ToArray();
+            _singletons.Clear();
+            
+            foreach (var singleton in oldSingletons)
+            {
+                singleton.ResetInner();
+            }
+        }
+    }
+    
+    public abstract class Singleton<T> : ISingleton, ISingletonBase where T : ISingleton, new()
     {
         protected static readonly ILogger _logger = LogManager.GetLogger(typeof(T).Name);
 
-        private static T _instance = new();
+        public static T Inst { get; private set; } = new();
 
-        public static T Inst => _instance;
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-        private static void ResetInner()
+        protected Singleton()
         {
+            SingletonCleaner.Register(this);
+        }
+        
+        public void ResetInner()
+        {
+            Inst = new T();
             Inst.Reset();
-            _instance = default;
         }
 
         public abstract void Reset();
