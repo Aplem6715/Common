11e10b8d39c0c154079f8d0a8913c3064bf221c3
Aplem6715 <Aplem6715@gmail.com>
2024-05-03 14:29:35 +0900

    Reactiveクラスの作成

---
+++ Reactive.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Aplem.Common
+{
+    [Serializable]
+    public class Reactive<T> where T : IEquatable<T>
+    {
+        public event Action<T> OnValueChanged;
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (value.Equals(_value))
+                    return;
+                _value = value;
+                OnValueChanged?.Invoke(_value);
+            }
+        }
+
+        [SerializeField] private T _value;
+
+        public Reactive(T defaultValue)
+        {
+            _value = defaultValue;
+        }
+
+        public WeakReference<Reactive<T>> ToWeak() => new(this);
+    }
+}
